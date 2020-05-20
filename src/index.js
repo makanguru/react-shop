a730f1707b7998d0892e6b92df167f71a8e8bb4f
unknown <makprg@ukr.net>
2020-05-20 10:58:13 +0300

    com2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './main.css'
 
+import { createStore, applyMiddleware } from 'redux'
+import { createBrowserHistory } from 'history'
+import thunk from 'redux-thunk'
+import { routerMiddleware } from 'connected-react-router'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+
+const history = createBrowserHistory()
+const middlewares = [thunk, routerMiddleware( history )]
+const store = createStore(
+    createRootReducer( history ),
+    composeWithDevTools(applyMiddleware(...middlewares))
+)
+
 document.write('Hello React/Redux!')
